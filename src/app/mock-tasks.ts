58b80eeea90136fa86598e316f4ba378073699ae
Pushkin <pushkinsharma0102@gmail.com>
2023-08-31 13:09:56 +0530

    Initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,32 @@
+
+import {Task} from './Task';
+export const TASKS :Task[] =[
+
+    {
+        id:1,
+        reminder:true,
+        text:'Doctors Appointment',
+        day:'May 5th at 2:30pm'
+
+    },
+    {
+        id:2,
+        reminder:true,
+        text:'Meeting at School',
+        day:'May 6th at 1:30pm'
+
+    },
+
+    {
+        id:3,
+        reminder:false,
+        text:'Food Shopping',
+        day:'May 7th at 12:30pm'
+
+    }
+
+
+
+
+
+]
